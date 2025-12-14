886243379d9ceb86c137df04084a32e5e376d49a
Sarujanan <sarujanang@gmail.com>
2025-12-14 07:18:23 +0530

    remove some files

--- server/backend/settings.py
+++ server/backend/settings.py
@@ -81,7 +81,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-DATABASES = {
+if USE_SQLITE:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
     'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
 }
 
